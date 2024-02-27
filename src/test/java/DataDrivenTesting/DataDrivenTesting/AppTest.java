1571f4fdff39cab3dc7f7b505d1757446dccf365
Atharva Deshmukh <atharvad34@gmail.com>
2024-02-27 15:30:21 +0530

    first Axis commit

---
+++ src/test/java/DataDrivenTesting/DataDrivenTesting/AppTest.java
@@ -0,0 +1,20 @@
+package DataDrivenTesting.DataDrivenTesting;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
